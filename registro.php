6d3a2df7349badf907f950b75f585556699dde8c
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-07-03 13:59:22 -0300

    Se agrega jquery y fuente de google

---
+++ registro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Crea una nueva cuenta</title>
+	<style rel="stylesheet" href="assets/css/estilos.css"></style>
+	<style rel="stylesheet" href="assets/fontawesome/css/all.css"></style>
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,900" rel="stylesheet">
+</head>
+<body>
+
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+</body>
+</html>
