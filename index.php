4ef69806d96ece6421ede455d20c4edcd239deec
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-07-03 16:49:58 -0300

    Se crea estructura inicial para el chat

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all.css"></link>
+	<link rel="stylesheet" type="text/css" href="assets/css/estilos.css"></link>
+	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,900" rel="stylesheet">
+</head>
+<body>
+	<nav id="nav">
+		NAV
+	</nav>
+
+	<div class="chat-container">
+		<section id="sidebar">
+			
+		</section><!-- fin sidebar -->
+		<section id="right-area">
+			<div class="messages">
+				MESSAGE
+			</div><!-- fin messages -->
+			<div class="chat-form">
+				FORM CHAT
+			</div><!-- fin chat-form -->
+		</section><!-- fin right-area -->
+	</div><!-- fin chat-container -->
+
+</body>
+</html>
