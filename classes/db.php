e2922a8c47e3b6ad6f61a577633bb750618bf023
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-07-04 01:55:27 -0300

    Se agrega clase base de datos y se hace función para autocarga de clases

---
+++ classes/db.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Db{
+
+	private $host = "localhost";
+	private $dbname = "mensajero";
+	private $username = "root";
+	private $password = "";
+	protected $con;
+
+	public function __construct(){
+
+		try{
+			//new PDO("mysql:host=localhost;dbname=mensajero","root", "");
+			$this->con = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username,$this->password);
+			echo "Coneccion exitosa";
+		}
+		catch(Exception $e){
+			echo "Problema de coneccion en la Base de Datos: ".$e->getMessage();
+		}
+	}
+}
--- registro.php
+++ registro.php
@@ -1,3 +1,11 @@
+<?php  
+
+include "init.php";
+
+$db = new Db;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
