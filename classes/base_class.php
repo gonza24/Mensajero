76dd328fa08fc931fd7739014a78d724d93c70b8
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-07-04 02:06:15 -0300

    Se crea clase base para consultas de base de datos

---
+++ classes/base_class.php
@@ -0,0 +1,22 @@
+<?php 
+
+class base_class extends Db{
+
+	private $Query;
+
+	public function normal_query($query, $param = null){
+
+		if(is_null($param)){
+			$this->Query = $this->con->prepare($query);
+			return $this->Query->execute();
+		}else{
+			$this->Query = $this->con->prepare($query);
+			return $this->Query->execute($param);
+		}
+	}
+
+	public function Count_Rows(){
+		return $this->Query->rowCount();
+	}
+
+}
